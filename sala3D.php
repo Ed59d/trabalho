bb792312e675e8df3a3a7d5a681503de63eb7f81
Ed59d <127413988+Ed59d@users.noreply.github.com>
2024-04-02 16:56:40 -0300

    v9rver
    continua

---
+++ sala3D.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+<table class="table table-hover">
+  <thead>
+
+    <div class="titulo">
+      
+    <h1>3°-desenvolvimento de sistemas</h1>
+    </div>
+
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">nome</th>
+      <th scope="col">ocorrencia</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Markk</td>
+      <td>ocorrncia</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>ocorrencia</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">13</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">14</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">15</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">16</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">17</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">18</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">19</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">20</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">21</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">22</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">23</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">24</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">25</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">26</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">27</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">28</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">29</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">30</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">31</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">32</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">33</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">34</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">35</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">36</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">37</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">38</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">39</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">40</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">41</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">42</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <th scope="row">43</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">44</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">45</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  
+
+  </tbody>
+</table>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>

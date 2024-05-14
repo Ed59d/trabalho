2dd3745d96b9cf8967a37e452b384bc281c7d22c
Ed59d <127413988+Ed59d@users.noreply.github.com>
2024-05-14 07:56:59 -0300

    v34

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="stilocad.css">
+    <link rel="stylesheet" href="inicio.css">
   <body>
   <div class="conteiner">
     <div class="row">
